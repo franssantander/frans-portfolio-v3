ba047428375692bb444af532d3ac36b27593b4e7
franssantander <santanderfrancisbeam@gmail.com>
2026-02-22 18:29:49 +0800

    wip hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import francisImage from "@/assets/francis-img.jpg";
 
 export default function Hero() {
   return (
-    <div className="grid w-full grid-cols-1 items-center justify-center py-32 lg:grid-cols-2">
+    <div className="grid w-full grid-cols-1 items-center justify-center py-20 lg:grid-cols-2 gap-y-4">
       <div className="flex flex-col items-center lg:items-start gap-4">
         <Avatar className="h-32 w-32 lg:h-64 lg:w-64">
           <AvatarImage
@@ -23,7 +23,18 @@ export default function Hero() {
         </p>
       </div>
 
-      <div className="hidden lg:block"></div>
+      <div className="space-y-3">
+        <div className="uppercase font-black text-center text-5xl space-y-3">
+          <h1>Full-Stack</h1>
+          <h1>Software Engineer</h1>
+          <h1>& UI/UX Designer</h1>
+        </div>
+        <p className="text-center text-neutral-500">
+          I specialize in building high quality application that applies good
+          practices, principles, architecture and user-friendly web applications
+          that deliver outstanding end user.
+        </p>
+      </div>
     </div>
   );
 }
